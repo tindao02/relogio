463f77ed99c28e5ad57588511658876200055cb8
Manoel Tindao <timdao02@hotmail.com>
2020-09-23 10:46:47 -0300

    Criação do projeto

---
+++ src/test/java/com/manoel/relogio/api/RelogioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoel.relogio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelogioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
