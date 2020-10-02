95365676f750cd91b52b061374aa59ee2d180ec9
Manoel Tindao <timdao02@hotmail.com>
2020-10-02 18:34:08 -0300

    Atualizações e acesso externo

--- src/main/java/com/manoel/relogio/api/controller/RelogioController.java
+++ src/main/java/com/manoel/relogio/api/controller/RelogioController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/relogios")
 @AllArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class RelogioController 
 {
 	private final RelogioService relogioService;
